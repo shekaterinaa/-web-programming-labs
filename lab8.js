90092cb5b83563c372c87326306e4297edd53020
Шкуропатова Екатерина <katygetty98@mail.ru>
2023-05-24 12:22:01 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -7,3 +7,11 @@ function showDate() {
     + 'Дата и время для греческой локали: ' + today.toLocaleString('el-GR') + '</br>' 
     + 'Дата и время для пакистанской локали: '  + today.toLocaleString('ur-PK') + '</br>'
     + 'Дата и время для китайской локали: ' + today.toLocaleString('zh-CN');}
+
+    function showDaysCount() { let today = new Date(); 
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value); let count = document.getElementById('count-days'); 
+    let daysCount = (today - birthday)/1000/60/60/24; 
+    daysCount = Math.floor(daysCount); 
+    count.innerHTML = 'Количество дней с даты рождения: ' + daysCount;
+}
